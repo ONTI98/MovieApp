366001660e2735d04647c6b1e9f73b0748732437
ONTI98 <157315560+ONTI98@users.noreply.github.com>
2025-06-09 10:27:29 +0200

    update style attribute on the hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,16 @@
-const Hero = ({text}) => {
+const Hero = ({ text, backdrop }) => {
   return (
     <>
-      <div className="container-fluid bg-dark ">
+      <div className="container-fluid bg-dark " id="hero-container">
         <div className="row ">
-          <div className="col  p-5">
-            <h1>{text}</h1>
+          <div className="col p-5">
+            <h1 >{text}</h1>
+            <div
+              id="hero-image"
+              style={{ backgroundImage: `url(${backdrop})` }}
+            ></div>
           </div>
+          <div className="backdrop-image"></div>
         </div>
       </div>
     </>
